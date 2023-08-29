389efcbbfd5d2677fa68c173379ee8e422d0c7c2
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-08-29 17:09:34 -0300

    Boton de Eliminar Tarea

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -9,30 +9,44 @@ export default function NewPage({ params }) {
   const [description, setDescription] = useState("");
 
   useEffect(() => {
-    fetch(`/api/tasks/${params.id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setTitle(data.title)
-        setDescription(data.description)
-      });
+    if (params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setTitle(data.title);
+          setDescription(data.description);
+        });
+    }
   }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const description = e.target.description.value;
 
-    const res = await fetch("api/tasks", {
-      method: "POST",
-      body: JSON.stringify({ title, description }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await res.json();
-    console.log(data);
+    if (params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application-json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+    } else {
+      const title = e.target.title.value;
+      const description = e.target.description.value;
 
+      const res = await fetch("api/tasks", {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+    }
+    router.refresh();
     router.push("/");
   };
 
@@ -61,9 +75,21 @@ export default function NewPage({ params }) {
           onChange={(e) => setDescription(e.target.value)}
           value={description}
         ></textarea>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
           Crear
         </button>
+
+        {params.id && (
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bond py-2 px-4 rounded ml-4"
+            type="button"
+          >
+            Delete
+          </button>
+        )}
       </form>
     </div>
   );
