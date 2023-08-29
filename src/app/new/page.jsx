e609775da3a71a4bc7b045ba8c8b36c191e111c9
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-08-29 16:46:18 -0300

    Formulario crear tarea

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,30 +1,40 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation"
-/**
- * ! Para eso funciona el useRoute para cuando termina quiero que me redirija a otra pagina 
- */
+import { useRouter } from "next/navigation"; //* !Para eso funciona el useRoute para cuando termina quiero que me redirija a otra pagina
+import { useEffect, useState } from "react";
 
-export default function NewPage() {
-  const router = useRouter()
+export default function NewPage({ params }) {
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-const onSubmit = async (e) => {
-  e.preventDefault()
-  const title = e.target.title.value
-  const description = e.target.description.value
+  useEffect(() => {
+    fetch(`/api/tasks/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTitle(data.title)
+        setDescription(data.description)
+      });
+  }, []);
 
-   const res = await fetch('api/tasks', {
-    method: 'POST',
-    body: JSON.stringify({title, description}),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    const data = await res.json()
-   console.log(data);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
 
-   router.push('/')
-  }
+    const res = await fetch("api/tasks", {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+
+    router.push("/");
+  };
 
   return (
     <div className="h-screen flex justify-center items-center">
@@ -37,13 +47,19 @@ const onSubmit = async (e) => {
           className="border border-gray-400 p-2 mb-4 w-full text-black"
           placeholder="titulo"
           type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
-        <label htmlFor="description" className="font-bold text-sm">Description tarea</label>
+        <label htmlFor="description" className="font-bold text-sm">
+          Description tarea
+        </label>
         <textarea
           id="description"
           rows="3"
           className="border border-gray-400 p-2 mb-4 w-full text-black"
           placeholder="Tu tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
         ></textarea>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Crear
