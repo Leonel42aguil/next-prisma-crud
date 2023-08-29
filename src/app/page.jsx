1ced46886db3bd15a5602efba7463d542a39c042
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-08-28 21:54:52 -0300

    Formulario Edit Tarea

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import TaskCard from "@/components/TaskCard";
 import { prisma } from "@/libs/prisma";
 
 async function loadTasks() {
@@ -12,12 +13,8 @@ export default async function HomePage() {
   return (
     <section className="container mx-auto">
       <div className="grid grid-cols-3 gap-3 mt-10">
-        {tasks.map((t) => (
-          <div key={t.id} className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer">
-            <h3 className="font-bold text-2xl mb-2">{t.title}</h3>
-            <p>{t.description}</p>
-            {/* <p>{new Date(t.createdAt).toLocaleDateString()}</p> */}
-          </div>
+        {tasks.map((task) => (
+          <TaskCard task={task} key={task.id} />
         ))}
       </div>
     </section>
